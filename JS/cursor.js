29859ba33afe24328abddfa1c1e8acc24206381d
FirstThinker_Swapnil <92979885+Swap-Nova@users.noreply.github.com>
2022-04-13 03:15:22 +0530

    Add files via upload

---
+++ JS/cursor.js
@@ -0,0 +1,17 @@
+// Written to understand the code in clear way
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500)
+})
+
+// This file is not imported to the webpage but the code is used in the index.html file under script tag
